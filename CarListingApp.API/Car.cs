c767741e4045cc3c146c413e906501bdf5af6af1
Sagar Chopra <schopra@firemate.com>
2023-08-31 16:48:56 +0530

    delete car api proj

--- CarListingApp.API/Car.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.ComponentModel.DataAnnotations;
-
-public class Car
-{
-    public int Id { get; set; }
-    public string Make { get; set; }
-    public string Model { get; set; }
-    public string Vin { get; set; }
-}
